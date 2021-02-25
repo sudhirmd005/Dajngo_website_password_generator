af91c5af285bcf5493ac94971763c49e01a75953
Sudhir <sudhir05.sd@gmail.com>
2021-02-25 14:37:40 +0530

    final5

--- pass_gen/settings.py
+++ pass_gen/settings.py
@@ -73,7 +73,7 @@ ROOT_URLCONF = 'pass_gen.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'pg/templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
